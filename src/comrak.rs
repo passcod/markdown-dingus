4c63b712e5b38916c3937693e5df07bc09001e3e
Asherah Connor <ashe@kivikakk.ee>
2025-12-24 19:33:40 +1100

    comrak: set unsafe to pass HTML through

--- src/comrak.rs
+++ src/comrak.rs
@@ -1,5 +1,8 @@
 use axum::response::Result;
 
 pub async fn render(input: &str) -> Result<String> {
-	Ok(comrak::markdown_to_html(input, &Default::default()))
+	let mut options = comrak::Options::default();
+	options.render.r#unsafe = true;
+
+	Ok(comrak::markdown_to_html(input, &options))
 }
